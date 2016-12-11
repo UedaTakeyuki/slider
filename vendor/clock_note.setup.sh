9c59f52d8ae0c47f83b58d20c557aad699a0c070
Ueda Takeyuki <ueda@latelierdueda.com>
2016-12-11 16:44:09 +0900

    clock_note.setup

--- vendor/clock_note.setup.sh
+++ vendor/clock_note.setup.sh
@@ -1,24 +1,4 @@
-# © Atelier UEDA 2016 - 
-
-# install requires
-# http://think-bowl.com/raspberry-pi/installing-the-think-bowl-i2c-libraries-for-python/
-
-mkdir quick2wire
-cd quick2wire
-here=`pwd`
-
-git clone https://github.com/quick2wire/quick2wire-python-api.git
-cd quick2wire-python-api
-sudo python3 setup.py install
-cd ..
-
-cd ..
-git clone https://bitbucket.org/thinkbowl/i2clibraries.git
-
-#
-# service file
-#
-cat << EOS1 > /etc/systemd/system/clock_note.service
+sudo sh -c 'cat << EOS1 > /etc/systemd/system/clock_note.service
 [Unit]
 Description=Sample Daemon
 After=rc-local.service
@@ -30,4 +10,20 @@ PIDFile=/var/run/clock_note.pid
 [Install]
 WantedBy=multi-user.target
 EOS1
+'
 
+sudo sh -c 'cat << EOS2 > /etc/systemd/system/clock_note2.service
+[Unit]
+Description=Sample Daemon
+After=rc-local.service
+[Service]
+ExecStart=/usr/bin/python /home/pi/SCRIPT/slider/clock_note2.py
+Restart=always
+Type=forking
+PIDFile=/var/run/clock_note.pid
+[Install]
+WantedBy=multi-user.target
+EOS2
+'
+
+sudo systemctl daemon-reload
