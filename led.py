2edda10c01692b3af7a007b4b6c26547895e2ca2
Ueda Takeyuki <ueda@latelierdueda.com>
2017-03-24 21:58:47 +0900

    fix Morse code

--- led.py
+++ led.py
@@ -98,163 +98,163 @@ class LED:
 
     def A(self, number): #number: LED number 0 or 1
         # A   ・－
-        led.short(number)
-        led.long(number)
+        self.short(number)
+        self.long(number)
 
     def B(self, number): #number: LED number 0 or 1
         # B   －・・・
-        led.long(number)
-        led.short(number)
-        led.short(number)
-        led.short(number)
+        self.long(number)
+        self.short(number)
+        self.short(number)
+        self.short(number)
         
-    def C(self, number): #number: LED number 0 or 1
+    def C(self, number): #number: self number 0 or 1
         # C   －・－・
-        led.long(number)
-        led.short(number)
-        led.long(number)
-        led.short(number)
+        self.long(number)
+        self.short(number)
+        self.long(number)
+        self.short(number)
         
-    def D(self, number): #number: LED number 0 or 1
+    def D(self, number): #number: self number 0 or 1
         # D   －・・
-        led.long(number)
-        led.short(number)
-        led.short(number)
+        self.long(number)
+        self.short(number)
+        self.short(number)
         
-    def E(self, number): #number: LED number 0 or 1
+    def E(self, number): #number: self number 0 or 1
         # E   ・
-        led.short(number)
+        self.short(number)
         
-    def F(self, number): #number: LED number 0 or 1
+    def F(self, number): #number: self number 0 or 1
         # F   ・・－・
-        led.short(number)
-        led.short(number)
-        led.long(number)
-        led.short(number)
+        self.short(number)
+        self.short(number)
+        self.long(number)
+        self.short(number)
         
-    def G(self, number): #number: LED number 0 or 1
+    def G(self, number): #number: self number 0 or 1
         # G   －－・
-        led.long(number)
-        led.long(number)
-        led.short(number)
+        self.long(number)
+        self.long(number)
+        self.short(number)
         
-    def H(self, number): #number: LED number 0 or 1
+    def H(self, number): #number: self number 0 or 1
         # H   ・・・・
-        led.short(number)
-        led.short(number)
-        led.short(number)
-        led.short(number)
+        self.short(number)
+        self.short(number)
+        self.short(number)
+        self.short(number)
         
-    def I(self, number): #number: LED number 0 or 1
+    def I(self, number): #number: self number 0 or 1
         # I   ・・
-        led.short(number)
-        led.short(number)
+        self.short(number)
+        self.short(number)
         
-    def J(self, number): #number: LED number 0 or 1
+    def J(self, number): #number: self number 0 or 1
         # J   ・－－－
-        led.short(number)
-        led.long(number)
-        led.long(number)
-        led.long(number)
+        self.short(number)
+        self.long(number)
+        self.long(number)
+        self.long(number)
         
-    def K(self, number): #number: LED number 0 or 1
+    def K(self, number): #number: self number 0 or 1
         # K   －・－
-        led.long(number)
-        led.short(number)
-        led.long(number)
+        self.long(number)
+        self.short(number)
+        self.long(number)
         
-    def L(self, number): #number: LED number 0 or 1
+    def L(self, number): #number: self number 0 or 1
         # L   ・－・・
-        led.short(number)
-        led.long(number)
-        led.short(number)
-        led.short(number)
+        self.short(number)
+        self.long(number)
+        self.short(number)
+        self.short(number)
         
-    def M(self, number): #number: LED number 0 or 1
+    def M(self, number): #number: self number 0 or 1
         # M   －－
-        led.long(number)
-        led.long(number)
+        self.long(number)
+        self.long(number)
         
-    def N(self, number): #number: LED number 0 or 1
+    def N(self, number): #number: self number 0 or 1
         # N   －・
-        led.long(number)
-        led.short(number)
+        self.long(number)
+        self.short(number)
         
-    def O(self, number): #number: LED number 0 or 1
+    def O(self, number): #number: self number 0 or 1
         # O   －－－
-        led.long(number)
-        led.long(number)
-        led.long(number)
+        self.long(number)
+        self.long(number)
+        self.long(number)
         
-    def P(self, number): #number: LED number 0 or 1
+    def P(self, number): #number: self number 0 or 1
         # P   ・－－・
-        led.short(number)
-        led.long(number)
-        led.long(number)
-        led.short(number)
+        self.short(number)
+        self.long(number)
+        self.long(number)
+        self.short(number)
         
-    def Q(self, number): #number: LED number 0 or 1
+    def Q(self, number): #number: self number 0 or 1
         # Q   －－・－
-        led.long(number)
-        led.long(number)
-        led.short(number)
-        led.long(number)
+        self.long(number)
+        self.long(number)
+        self.short(number)
+        self.long(number)
         
-    def R(self, number): #number: LED number 0 or 1
+    def R(self, number): #number: self number 0 or 1
         # R   ・－・
-        led.short(number)
-        led.long(number)
-        led.short(number)
+        self.short(number)
+        self.long(number)
+        self.short(number)
         
-    def S(self, number): #number: LED number 0 or 1
+    def S(self, number): #number: self number 0 or 1
         # S   ・・・
-        led.short(number)
-        led.short(number)
-        led.short(number)
+        self.short(number)
+        self.short(number)
+        self.short(number)
         
-    def T(self, number): #number: LED number 0 or 1
+    def T(self, number): #number: self number 0 or 1
         # T   －
-        led.long(number)
+        self.long(number)
 
-    def U(self, number): #number: LED number 0 or 1
+    def U(self, number): #number: self number 0 or 1
         # U   ・・－
-        led.short(number)
-        led.short(number)
-        led.long(number)
+        self.short(number)
+        self.short(number)
+        self.long(number)
 
-    def V(self, number): #number: LED number 0 or 1
+    def V(self, number): #number: self number 0 or 1
         # V   ・・・－
-        led.short(number)
-        led.short(number)
-        led.short(number)
-        led.long(number)
+        self.short(number)
+        self.short(number)
+        self.short(number)
+        self.long(number)
 
-    def W(self, number): #number: LED number 0 or 1
+    def W(self, number): #number: self number 0 or 1
         # W   ・－－
-        led.short(number)
-        led.long(number)
-        led.long(number)
+        self.short(number)
+        self.long(number)
+        self.long(number)
 
-    def X(self, number): #number: LED number 0 or 1
+    def X(self, number): #number: self number 0 or 1
         # X   －・・－
-        led.long(number)
-        led.short(number)
-        led.short(number)
-        led.long(number)
+        self.long(number)
+        self.short(number)
+        self.short(number)
+        self.long(number)
 
-    def Y(self, number): #number: LED number 0 or 1
+    def Y(self, number): #number: self number 0 or 1
         # Y   －・－－
-        led.long(number)
-        led.short(number)
-        led.long(number)
-        led.long(number)
+        self.long(number)
+        self.short(number)
+        self.long(number)
+        self.long(number)
 
-    def Z(self, number): #number: LED number 0 or 1
+    def Z(self, number): #number: self number 0 or 1
         # Z   －－・・
-        led.long(number)
-        led.long(number)
-        led.short(number)
-        led.short(number)
+        self.long(number)
+        self.long(number)
+        self.short(number)
+        self.short(number)
 
 if __name__ == '__main__':
     led = LED()
