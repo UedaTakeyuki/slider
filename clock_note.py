a9ff6f324adbbe12d2fa683a934a1dbd1550adcb
Ueda Takeyuki <ueda@latelierdueda.com>
2017-04-03 17:23:56 +0900

    remove redandant commeted out lines.

--- clock_note.py
+++ clock_note.py
@@ -1,12 +1,7 @@
 # coding:utf-8 Copy Right Atelier Ueda © 2016 -
 #
-# i2c_lcd install: http://think-bowl.com/raspberry-pi/installing-the-think-bowl-i2c-libraries-for-python/
-# i2c_lcd usage: http://think-bowl.com/raspberry-pi/i2c-python-library-lcd-with-the-raspberry-pi/
 import os
 import sys
-#sys.path.append("/home/osmc/MySCRIPT")
-#sys.path.append("/home/osmc/MySCRIPT/quick2wire-python-api")
-#sys.path.append(os.getcwd()+"/vendor/quick2wire-python-api/quick2wire")
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/vendor")
 from i2clibraries import i2c_lcd
 import quick2wire
@@ -18,7 +13,6 @@ import traceback
 import inspect
 import requests
 import configparser
-#import grequests
 
 # 定数
 configfile = os.path.dirname(os.path.abspath(__file__))+'/clock_note.ini'
@@ -31,7 +25,6 @@ i2c_addr = p.stdout.readline().strip().decode('utf-8')
 #print (i2c_addr)
 
 logging.basicConfig(format='%(asctime)s %(filename)s %(lineno)d %(levelname)s %(message)s',filename='/home/pi/LOG/clock_note.engine.log',level=logging.DEBUG)
-#lcd = i2c_lcd.i2c_lcd(int(ini.get("lcd", "i2c_addr"),0),0, 2, 1, 0, 4, 5, 6, 7, 3)
 lcd = i2c_lcd.i2c_lcd(int("0x" + i2c_addr,0),0, 2, 1, 0, 4, 5, 6, 7, 3)
 
 def msg_log(msg_str):
@@ -44,30 +37,11 @@ def msg_err_log(msg_str):
 
 def say(phrase):
 	try:
-	#p = subprocess.Popen("/home/pi/install/aquestalkpi/AquesTalkPi "+str+" | aplay",
-	#p = subprocess.check_call('sudo sh -c "/home/pi/install/aquestalkpi/AquesTalkPi '+str+'| aplay"',
-		#command = '/usr/bin/curl -F "phrase='+phrase.decode('utf-8')+'" localhost/say.php'
-#		command = '/usr/bin/curl -F "phrase='+phrase+'" localhost/say.php'
-#		command_str = command.encode('utf-8')
-#		print (command_str)
 		if ini.get("path", "say_path"): # settings is NOT null then
 			payload = {'phrase': phrase}
-			#r = requests.post('http://localhost/say.php', data=payload, timeout=10, verify=False)
 			r = requests.post(ini.get("path", "say_path"), data=payload, timeout=10, verify=False)
-#		urls = ['http://localhost/say.php']
-#		rs = (grequests.post('http://localhost/say.php', data=payload, timeout=10, verify=False) for u in urls)
-#		grequests.map(rs)
-		#print (command)
-#		p = subprocess.Popen(command,
-#		p = subprocess.Popen(command_str,
-#												stdout=subprocess.PIPE,
-#												shell=True)
 	except:
 		msg_err_log(traceback.format_exc())
-#		info=sys.exc_info()
-#		msg_err_log ("Unexpected error:"+ traceback.format_exc(info[0]))
-#		msg_err_log (traceback.format_exc(info[1]))
-#		msg_err_log (traceback.format_exc(info[2]))
 
 def current_ip():
 	p = subprocess.Popen("hostname -I",
@@ -92,11 +66,9 @@ def show_ip(sec):
 
 def show_temp(sec):
 	global lcd
-#	p = subprocess.Popen("tail -n 1 /boot/DATA/log/temp.csv",
 	p = subprocess.Popen("tail -n 1 "+ini.get("data", "temp_path")+"/temp.csv",
 												stdout=subprocess.PIPE,
 												shell=True)
-#	result = p.stdout.readline().strip().decode('utf-8').split(',')
 	result = p.stdout.readline().strip().decode('utf-8','ignore').split(',')
 	lcd.home()
 	lcd.clear()
@@ -106,7 +78,6 @@ def show_temp(sec):
 
 def show_humidity(sec):
 	global lcd
-#	p = subprocess.Popen("tail -n 1 /boot/DATA/log/humidity.csv",
 	p = subprocess.Popen("tail -n 1 "+ini.get("data", "humidity_path")+"/humidity.csv",
 												stdout=subprocess.PIPE,
 												shell=True)
@@ -119,7 +90,6 @@ def show_humidity(sec):
 
 def show_humiditydeficit(sec):
 	global lcd
-#	p = subprocess.Popen("tail -n 1 /boot/DATA/log/humiditydeficit.csv",
 	p = subprocess.Popen("tail -n 1 "+ini.get("data", "humiditydeficit_path")+"/humiditydeficit.csv",
 												stdout=subprocess.PIPE,
 												shell=True)
@@ -127,13 +97,11 @@ def show_humiditydeficit(sec):
 	lcd.home()
 	lcd.clear()
 	lcd.writeString("HumidDef = " + result[1])
-#	say("飽差"+result[1])
 	time.sleep(sec)
 
 def show_CO2(sec):
 	global lcd
 	if ini.get("data", "CO2_path"): # settings is NOT null then
-	#	p = subprocess.Popen("tail -n 1 /boot/DATA/log/co2.csv",
 		p = subprocess.Popen("tail -n 1 "+ini.get("data", "CO2_path")+"/CO2.csv",
 													stdout=subprocess.PIPE,
 													shell=True)
@@ -167,7 +135,6 @@ def main():
 		now = datetime.datetime.now()
 		now_str = datetime.datetime.now().strftime('%m-%d %H:%M:%S')
 		if (now.second == 1):
-#			pass
 			if not is_said:
 				say(str(now.hour) + "時" + str(now.minute) + "分です")
 				is_said = True
@@ -176,7 +143,6 @@ def main():
 		
 		if (datetime.datetime.now().second == 31):
 			show_ip(2)
-#			show_temp(2)
 			show_temp(3) # openjtalk が間に合わない
 			show_humidity(2)
 			show_humiditydeficit(2)
