2bc29ba8d83a5ab0809c7199afe55c3798558547
Ueda Takeyuki <ueda@latelierdueda.com>
2017-05-07 12:55:56 +0900

    compoud

---
+++ compound_read.sh
@@ -0,0 +1,5 @@
+sudo ntpdate -u time.google.com
+sudo python /home/pi/install/efota/fota.py
+sudo python /home/pi/SCRIPT/efota/fota.py
+sudo python /home/pi/SCRIPT/statelog/statelog.py
+sudo python /home/pi/SCRIPT/slider/read.py
